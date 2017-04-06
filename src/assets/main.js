e53bd3bb13ba15e8f979b4f8211c977ba8fc3803
JoscelynJames <josce.james7@hotmail.com>
2017-04-05 21:39:08 -0600

    add ajax call and displayed on page

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
-  // your code will go here
-
+  $.ajax({
+    url: 'https://www.codeschool.com/users/bradford_hamilton.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      for (var i = 0; i < response.courses.completed.length; i++) {
+        $('#badges').append(
+          '<div class="course">' +
+          '<h3>' + response.courses.completed[i].title + '</h3>' +
+          '<img src="' + response.courses.completed[i].badge + '"/> ' +
+          '<a href="' + response.courses.completed[i].url + '" target="_blank" class="btn btn-primary">See Course</a>' +
+          '</div>'
+        )
+      }
+    },
+    error: function(error) {
+      console.log('This is the error from the ajax, sir:', error);
+    }
+  });
+  
 });
